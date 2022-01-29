0f6f7da1db4cab5aa9a4077e7c66c54db23985fd
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2022-01-29 22:37:01 +0530

    add inline form to add a movie to the watchlist of the logged in user

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,23 @@
 @extends('base')
 
 @section('content')
+<form class="row row-cols-lg-auto g-3 align-items-center mt-3" action="{{ url('watchlist') }}" method="post">
+    @csrf
+    <div class="col-12">
+        <label class="visually-hidden" for="title">Title</label>
+        <input class="form-control @error('title') is-invalid @enderror" type="text" name="title" id="title" placeholder="Movie title" aria-describedby="validationServerTitleFeedback">
+        @error('title')
+        <div id="validationServerTitleFeedback" class="invalid-feedback">{{ $message }}</div>
+        @enderror
+    </div>
+
+    <!-- TODO: add the year field -->
+
+    <div class="col-12">
+        <button type="submit" class="btn btn-sm btn-outline-primary">Add movie to watchlist</button>
+    </div>
+</form>
+
 @isset($moviesInWatchlist)
 <ol>
     @foreach($moviesInWatchlist as $watchlistitem)
