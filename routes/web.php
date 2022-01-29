1f5b3ee0a4f6f0dbf31e3f54d63a1c8fb0daf8c2
Arjun Krishna Babu <arjunkrishnababu96@gmail.com>
2022-01-29 17:58:26 +0530

    declare route to signup page

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('auth')->group(function () {
+    Route::prefix('signup')->group(function () {
+        Route::get('', fn () => view('auth.signup'));
+    });
+});
